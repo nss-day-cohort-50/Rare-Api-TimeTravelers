5e8e531d35f467dffb14f758127caf181ea8e369
Blake Rhoden <zbrhoden@gmail.com>
2021-11-17 11:27:23 -0600

    Post fixed?

--- rarerestapi/views/post_view.py
+++ rarerestapi/views/post_view.py
@@ -9,32 +9,29 @@ from rarerestapi.models import RareUsers, Posts, Categories
 from rest_framework.decorators import action
 
 class PostView(ViewSet):
-    def create(self, request):
-        author = RareUsers.objects.get(user=request.auth.user)
-        post = Posts.objects.get(pk=request.data['gameId'])
+    # def create(self, request):
+    #     author = RareUsers.objects.get(user=request.auth.user)
+    #     post = Posts.objects.get(pk=request.data['gameId'])
 
-        try:               
-            post = Posts.objects.create(
-                title = request.data["title"],
-                user_id = models.IntegerField("RareUsers")
-                category_id = models.IntegerField("Categories")
-                publications_date=request.data[]
-                content=request.data['content'],
-                approved=request.data['approved'],
-                image_url=request.data['image_url']
-            )
-            post_serializer = PostSerializer(post, context={'request': request})
-            return Response(post_serializer.data, status=status.HTTP_201_CREATED)
-        except ValidationError as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+    #     try:               
+    #         post = Posts.objects.create(
+    #             title = request.data["title"],
+    #             user_id = models.IntegerField("RareUsers")
+    #             category_id = models.IntegerField("Categories")
+    #             publications_date=request.data[]
+    #             content=request.data['content'],
+    #             approved=request.data['approved'],
+    #             image_url=request.data['image_url']
+    #         )
+    #         post_serializer = PostSerializer(post, context={'request': request})
+    #         return Response(post_serializer.data, status=status.HTTP_201_CREATED)
+    #     except ValidationError as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request):
         post = Posts.objects.get(user=request.auth.user)
         posts = post.objects.all()
 
-    
-        for post in posts:
-            post.joined = post in post.attendees.all()
         game = self.request.query_params.get('gameId', None)
         if game is not None:
             posts = posts.filter(game__id=type)
@@ -49,6 +46,8 @@ class PostView(ViewSet):
         post_serializer = PostSerializer(post, context={'request': request})
         return Response(post_serializer.data)
 
+
+class PostSerializer(ModelSerializer):
     class Meta:
         model = Posts
         fields = ['user', 'title', 'category', 'publication_date', 'approved', 'content']
